29b32bfff09dccbf4ef769183178b8f9ef34587f
Jon Gutschon <jongutschon123@gmail.com>
2021-09-28 17:05:00 -0400

    update inner loop logic and cRIO file

--- formulaModule.c
+++ formulaModule.c
@@ -1,4 +1,3 @@
-float PI = 3.14159;
 float C_1 = 1;
 float K_1 = 1;
 float TAU = 1;
@@ -9,27 +8,31 @@ float TAU = 1;
 /*  Variables may be declared on the box border, as shown for the input 
 "Tms" and the output "BallPosn". Variables can also be declared inline as was done for "Temp1". */
 
+float Temp1;
 float eGearAng;
+float PI = 3.14159;
 
-/*  Shift registers permit previous values of variables to be saved.
-The output variable "e" is wired to a shift register input on the For Loop border.
+/*  Shift registers permit previous values of variables to be saved. 
+The output variable "e" is wired to a shift register input on the For Loop border.  
 The inputs "e1" and "e2"are wired  to the corresponding shift register outputs.
-"e1" holds the value of "e" from the previous iteration and "e2" holds the value of "e1" from  the previous iteration. */
+ "e1" holds the value of "e" from the previous iteration and "e2" holds the value of "e1" from  the previous iteration. */
 
 /* Place your sensor SCALING here */
 /* NO scaling is provided for the demo */
 BallPosn = posV ;  /* V to V */
+
 // Gear angle in radians
-ServoAng = (angV - 4.78) / (3.65 - 4.78) * (PI/2) - (PI/4); /* V to V */
+ServoAng = (angV - 4.765) / (3.66 - 4.765) * (PI/2) - (PI/4); /* V to V */
 /* SCALING end */
 
-if (Loop  < 3) {   /* all shift registers cleared after 3rd iteration; this statement initializes the shift registers */
-  u = e = ThRef = posV= angV =ServoAng= BallPosn= 0;
+if (Loop < 3) {   /* all shift registers cleared after 3rd iteration; this statement initializes the shift registers */
+  u = e = ThRef = posV = angV = ServoAng = BallPosn = 0;
 } else {
   if (Manual) {   /*manual motor voltage control*/
     u = MotV;
   } else {    /*control algorithm*/
-  
+    ThRef = ref;
+
     /* CAUTION: DO NOT load the output of a nonlinear block (e.g., saturator, offset) into a SHIFT REGISTER, 
     to avoid introducing a nonlinearity into your controller loop. Create separate variables to hold nonlinear values.*/
 
@@ -37,9 +40,16 @@ if (Loop  < 3) {   /* all shift registers cleared after 3rd iteration; this stat
     BallPosn = 0; // REMOVE this line when the ball is being used on the beam
 
     /* Place your gear angle SATURATOR below */
+    //if (ThRef > PI / 4) {ThRef = PI/4;}
+    //else if ( ThRef < -PI / 4) {ThRef = -PI/4;}
 
     /* Place your inner loop GEAR ANGLE CONTROLLER below */
-    u = ServoAng - ref;
+    u = -15 * (ThRef - ServoAng);/*
+    if (u > 6.0) {
+      u = 6.0;
+    } else if (u < -6.0) {
+      u = -6.0;
+    } */
   }
 }
     
