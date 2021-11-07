3167023d5f98800168e7828b16a3f175bd38e186
dlrobson <danr.236@gmail.com>
2021-11-07 12:38:59 -0500

    cleaned up code a bit

--- formulaModule.c
+++ formulaModule.c
@@ -1,48 +1,41 @@
-// C_1 = ( -1.204931478672254 * z + 1.032681063701449) / (z -0.960962180870512)
-// Controller Coefficients
+// Controller Coefficients Constants. Controller is given by:
+// C_1 = (A_0 * z + A_1)/(B_0 * z + B_1)
 float A_0 = -1.204931478672254;
 float A_1 = 1.032681063701449;
 float B_0 = 1.000000000000000;
 float B_1 = -0.960962180870512;
 
+// Measurement Scaling Constants
 float V_neg_45 = 4.765;
 float V_pos_45 = 3.650;
 float BALL_LEFT_V = 3.290;
 float BALL_RIGHT_V = 7.320;
 float BEAM_LENGTH = 0.417;
 
-/*  Insert below the code for your scaling, saturation block, and
-   controllers.*/
-/*  Shift registers permit previous values of variables to be saved.
-The output variable "e" is wired to a shift register input on the For Loop
-border.
-The inputs "e1" and "e2"are wired  to the corresponding shift register
-outputs.
- "e1" holds the value of "e" from the previous iteration and "e2" holds the
-value of "e1" from  the previous iteration. */
+// Measured Constants
+float K_2 = 0.0573;
 
-/* Place your sensor SCALING here */
-/* NO scaling is provided for the demo */
+// Constraint Constants
+float THETA_MAX = 0.7;
+float THETA_MIN = -0.7;
 
-// Ball position in m
-BallPosn = BEAM_LENGTH / (BALL_RIGHT_V - BALL_LEFT_V) * (posV - BALL_LEFT_V); 
-
-// Gear angle in radians
+// Ball Position in m
+BallPosn = BEAM_LENGTH / (BALL_RIGHT_V - BALL_LEFT_V) * (posV - BALL_LEFT_V);
+// Gear Angle in RAD
 ServoAng = (angV - V_neg_45) / (V_pos_45 - V_neg_45) * (pi / 2) - (pi / 4);
 
-/**************** SCALING END ****************/
-
 if (Loop < 3) {
-  /* all shift registers cleared after 3rd iteration; this
-                    statement initializes the shift registers */
+  // All shift registers cleared after 3rd iteration. This statement
+  // initializes the shift registers
   u = e = ThRef = posV = angV = ServoAng = BallPosn = 0;
 } else {
-  if (Manual) {/*manual motor voltage control*/
+  if (Manual) {
+    // MANUAL MOTOR VOLTAGE CONTROL
     u = MotV;
-  } else { /*control algorithm*/
+  } else {
+    // CONTROL ALGORITHM
     ThRef = ref;
-    
-    float phi = 0.0560 * ThRef;
+    float phi = K_2 * ThRef;
 
     /* CAUTION: DO NOT load the output of a nonlinear block (e.g., saturator,
     offset) into a SHIFT REGISTER,
@@ -50,21 +43,16 @@ if (Loop < 3) {
     separate variables to hold nonlinear values.*/
 
     /* Place your outer loop BALL POSITION CONTROLLER below */
-    
-    /* Place your gear angle SATURATOR below */
-    if (ThRef > 0.7) {
-      ThRef = 0.7;
-    } else if (ThRef < -0.7) {
-      ThRef = -0.7;
+
+    // GEAR ANGLE SATURATOR
+    if (ThRef > THETA_MAX) {
+      ThRef = THETA_MAX;
+    } else if (ThRef < THETA_MIN) {
+      ThRef = THETA_MIN;
     }
 
+    // INNER LOOP GEAR ANGLE CONTROLLER
     e = ThRef - ServoAng;
-
-    /* Place your inner loop GEAR ANGLE CONTROLLER below */
     u = 1 / B_0 * (-B_1 * u1 + A_0 * e + A_1 * e1);
   }
 }
-
-/* ThRef, ThRef1, e, e1 are present, but not used in this demo.
-However, they will be necessary (at a minimum) when the controllers will be
-implemented. */
