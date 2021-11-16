169af63b04117bb0d717df76c6c9bbc4ca6ee853
dlrobson <danr.236@gmail.com>
2021-11-16 15:48:28 -0500

    nov16 lab session

--- formulaModule.c
+++ formulaModule.c
@@ -1,19 +1,23 @@
 // Controller Coefficients Constants. Controller is given by:
-// C_1 = (A_0 * z + A_1)/(B_0 * z + B_1)
-float A_0 = -1.204931478672254;
-float A_1 = 1.032681063701449;
-float B_0 = 1.000000000000000;
-float B_1 = -0.960962180870512;
+// C_1 = (A1_0 * z + A1_1)/(B1_0 * z + B1_1)
+float A1_0 = -1.204931478672254;
+float A1_1 = 1.032681063701449;
+float B1_0 = 1.000000000000000;
+float B1_1 = -0.960962180870512;
+
+// C_2 = (A2_0 * z + A2_1)/(B2_0 * z + B2_1)
+float A2_0 = -7.000000000000000;
+float A2_1 = 6.997553059949510;
+float B2_0 = 1.000000000000000;
+float B2_1 = -0.997503122397460;
 
 // Measurement Scaling Constants
 float V_neg_45 = 4.765;
 float V_pos_45 = 3.650;
 float BALL_LEFT_V = 3.290;
 float BALL_RIGHT_V = 7.320;
-float BEAM_LENGTH = 0.417;
-
-// Measured Constants
-float K_2 = 0.0573;
+float RADIUS_BALL = 0.0127;
+float BEAM_LENGTH = 0.417 - 2 * RADIUS_BALL;
 
 // Constraint Constants
 float THETA_MAX = 0.7;
@@ -27,15 +31,13 @@ ServoAng = (angV - V_neg_45) / (V_pos_45 - V_neg_45) * (pi / 2) - (pi / 4);
 if (Loop < 3) {
   // All shift registers cleared after 3rd iteration. This statement
   // initializes the shift registers
-  u = e = ThRef = posV = angV = ServoAng = BallPosn = 0;
+  u = e = ePos = ThRef = posV = angV = ServoAng = BallPosn = 0;
 } else {
   if (Manual) {
     // MANUAL MOTOR VOLTAGE CONTROL
     u = MotV;
   } else {
     // CONTROL ALGORITHM
-    ThRef = ref;
-    float phi = K_2 * ThRef;
 
     /* CAUTION: DO NOT load the output of a nonlinear block (e.g., saturator,
     offset) into a SHIFT REGISTER,
@@ -43,16 +45,19 @@ if (Loop < 3) {
     separate variables to hold nonlinear values.*/
 
     /* Place your outer loop BALL POSITION CONTROLLER below */
+    ePos = ref - BallPosn;
+    ThRef = 1 / B2_0 * (-B2_1 * ThRef1 + A2_0 * ePos + A2_1 * ePos1);
 
+    float ThRefSaturated = ThRef;
     // GEAR ANGLE SATURATOR
-    if (ThRef > THETA_MAX) {
-      ThRef = THETA_MAX;
-    } else if (ThRef < THETA_MIN) {
-      ThRef = THETA_MIN;
+    if (ThRefSaturated > THETA_MAX) {
+      ThRefSaturated = THETA_MAX;
+    } else if (ThRefSaturated < THETA_MIN) {
+      ThRefSaturated = THETA_MIN;
     }
 
     // INNER LOOP GEAR ANGLE CONTROLLER
-    e = ThRef - ServoAng;
-    u = 1 / B_0 * (-B_1 * u1 + A_0 * e + A_1 * e1);
+    e = ThRefSaturated - ServoAng;
+    u = 1 / B1_0 * (-B1_1 * u1 + A1_0 * e + A1_1 * e1);
   }
 }
--- paramGen.m
+++ paramGen.m
@@ -1,5 +1,12 @@
-clear all;
-close all;
+clear all; close all;
+
+T = 0.001;
+K_1 = -2.185;  % rad/Vs
+K_2 = 0.0609;  % rad/rad
+K_3 = -6.0885; % m / (s^2 * rad)
+TAU = 0.0178;  % s
+
+%%%% LAB 1 %%%%
 OS_dec = 0.05;
 T_SETTLING = 0.25;
 
@@ -7,10 +14,6 @@ ZETA_MAX = -log(OS_dec)/sqrt(pi^2 + log(OS_dec)^2);
 THETA_MAX = rad2deg(acos(ZETA_MAX));
 RE_S_MAX = -4 / T_SETTLING;
 
-K_1 = -2.185;  % rad/Vs
-TAU = 0.0178;  % s
-
-%%%%% FIRST-ORDER C(s) %%%%%
 % Because P has pole at 0, that means that e_ss will reach 0
 DOM_POLE_RE = RE_S_MAX * 4; % note this should be negative
 DOM_POLE = [1 -DOM_POLE_RE];
@@ -28,49 +31,32 @@ A = [1 0 0 0;
     0 1/TAU K_1/TAU 0;
     0 0 0 K_1/TAU];
 
-C_COEFF = linsolve(A, CP_DES);
+C_1_COEFF = linsolve(A, CP_DES);
 
-P = tf([K_1/TAU], [1 1/TAU 0]);
-C = tf([C_COEFF(3) C_COEFF(4)], [C_COEFF(1) C_COEFF(2)]);
+P_motor = tf([K_1], [TAU 1 0]);
+C_1 = tf([C_1_COEFF(3) C_1_COEFF(4)], [C_1_COEFF(1) C_1_COEFF(2)]);
+D_1 = c2d(C_1, T);
+
+% THETA_REF_TO_THETA = C_1 * P_motor / (1 + C_1 * P_motor);
+THETA_TO_PHI = K_2;
+PHI_TO_Y = tf([K_3], [1, 0, 0]);
+% INNER_LOOP_PLANT = THETA_REF_TO_THETA * THETA_TO_PHI * PHI_TO_Y;
+% P = INNER_LOOP_PLANT;
+
+C_2 = -7 * tf([1, 0.35], [1, 2.5]);
 
 % SIMULINK PARAMETERS
-T = 0.001;
-D = c2d(C, T);
-tfinal = 4.5;
+D = c2d(C_2, T);
+position_offset = 0.15;     % m
+position_amplitude = 0.1;   % 0.15 - 0.25 m
+tfinal = 100.0;
 sim('general_SD_model.slx', tfinal);
-V_minimum = min(Uk)
-V_maximum = max(Uk);
-range = V_maximum - V_minimum;
-
-% plot
-% experimental data
-exp_angle = xlsread('7Vwithoutbeam.xlsx', 'Sheet1', 'C2:C10002');
-exp_time = xlsread('7Vwithoutbeam.xlsx', 'Sheet1', 'A2:A10002');
-exp_ref = xlsread('7Vwithoutbeam.xlsx', 'Sheet1', 'B2:B10002');
-
-start = 1500;
-size = tfinal*1000;
-plot(t_sim(start:size), Ysd(start:size));
-hold on;
-plot(exp_time(start:size), exp_angle(start:size));
-hold on;
-plot(exp_time(start:size), exp_ref(start:size), 'Color', 'k');
-
-title('Sampled-Data System Response - Simulated vs. Experimental');
-ylabel('Gear Angle (rad)');
-xlabel('Time (sec)');
-xlim([start/1000 tfinal])
-legend('Simulated', 'Experimental', 'Reference Signal', 'Location', 'southeast');
-grid on;
-
-
-% Output variables
-last_2percent = find(Ysd < 0.672 | Ysd > 0.728, 1, 'last')
+% plot(t_sim, y, t_sim, y_error, t_sim, y_ref)
 
 format long
 [NUM,DEN]=tfdata(D,'v');
 
-fprintf('float A_0 = %.15f;\n', NUM(1));
-fprintf('float A_1 = %.15f;\n', NUM(2));
-fprintf('float B_0 = %.15f;\n', DEN(1));
-fprintf('float B_1 = %.15f;\n', DEN(2));
+fprintf('float A2_0 = %.15f;\n', NUM(1));
+fprintf('float A2_1 = %.15f;\n', NUM(2));
+fprintf('float B2_0 = %.15f;\n', DEN(1));
+fprintf('float B2_1 = %.15f;\n', DEN(2));
