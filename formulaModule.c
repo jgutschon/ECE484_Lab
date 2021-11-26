2ebdda5523f21dc5aac4e06b7f0538ac9205d929
Jon Gutschon <46715495+jgutschon@users.noreply.github.com>
2021-11-26 10:15:28 -0500

    update ballPosn formula with radius

--- formulaModule.c
+++ formulaModule.c
@@ -14,24 +14,14 @@ float B_LEAD_1 = -0.997503122397460;
 // PART C CONTROLLER
 // C_2 = (A2_0 * z^3 + A2_1 * z^2 + A2_2 * z + A2_3)
 //       / (B2_0 * z^3 + B2_1 * z^2 + B2_2 * z + B2_3)
-// float A2_0 = -0.295404446074768;
-// float A2_1 = 0.295232270026272;
-// float A2_2 = 0.295404414174432;
-// float A2_3 = -0.295232301926608;
-// float B2_0 = 1.000000000000000;
-// float B2_1 = -2.979638314061477;
-// float B2_2 = 2.959401373706974;
-// float B2_3 = -0.979763059645498;
-
-// PART E CONTROLLER
-// float A2_0 = 0.000000000000000;
-// float A2_1 = -0.110664622404449;
-// float A2_2 = 0.221261353288049;
-// float A2_3 = -0.110596745599437;
-// float B2_0 = 1.000000000000000;
-// float B2_1 = -2.991121832548782;
-// float B2_2 = 2.982272333037067;
-// float B2_3 = -0.991150500488285;
+float A2_0 = -0.295404446074768;
+float A2_1 = 0.295232270026272;
+float A2_2 = 0.295404414174432;
+float A2_3 = -0.295232301926608;
+float B2_0 = 1.000000000000000;
+float B2_1 = -2.979638314061477;
+float B2_2 = 2.959401373706974;
+float B2_3 = -0.979763059645498;
 
 // Measurement Scaling Constants
 float V_neg_45 = 4.765;
@@ -45,8 +35,14 @@ float BEAM_LENGTH = 0.417 - 2 * RADIUS_BALL;
 float THETA_MAX = 0.7;
 float THETA_MIN = -0.7;
 
+// Self-defined Constants
+float MIN_STICTION_RANGE = 0.001;
+float MAX_STICTION_RANGE = 0.100;
+float BALL_STICTION_OFFSET = 0.05;
+
 // Ball Position in m
-BallPosn = BEAM_LENGTH / (BALL_RIGHT_V - BALL_LEFT_V) * (posV - BALL_LEFT_V);
+BallPosn = BEAM_LENGTH / (BALL_RIGHT_V - BALL_LEFT_V) * (posV - BALL_LEFT_V) + RADIUS_BALL;
+
 // Gear Angle in RAD
 ServoAng = (angV - V_neg_45) / (V_pos_45 - V_neg_45) * (pi / 2) - (pi / 4);
 
@@ -76,16 +72,23 @@ if (Loop < 3) {
             (-B2_1 * ThRef1 - B2_2 * ThRef2 - B2_3 * ThRef3 + A2_0 * ePos +
              A2_1 * ePos1 + A2_2 * ePos2 + A2_3 * ePos3);
 
-    float ThRefSaturated = ThRef;
+    float ThRefAdjusted = ThRef;
+    
+    if (abs(ePos) > MIN_STICTION_RANGE && abs(ePos) < MAX_STICTION_RANGE)
+    {
+      int e_sgn = (ePos > 0) - (ePos < 0);
+      ThRefAdjusted += e_sgn * -BALL_STICTION_OFFSET;
+    }
+
     // GEAR ANGLE SATURATOR
-    if (ThRefSaturated > THETA_MAX) {
-      ThRefSaturated = THETA_MAX;
-    } else if (ThRefSaturated < THETA_MIN) {
-      ThRefSaturated = THETA_MIN;
+    if (ThRefAdjusted > THETA_MAX) {
+      ThRefAdjusted = THETA_MAX;
+    } else if (ThRefAdjusted < THETA_MIN) {
+      ThRefAdjusted = THETA_MIN;
     }
 
     // INNER LOOP GEAR ANGLE CONTROLLER
-    e = ThRefSaturated - ServoAng;
+    e = ThRefAdjusted - ServoAng;
     u = 1 / B1_0 * (-B1_1 * u1 + A1_0 * e + A1_1 * e1);
   }
 }
