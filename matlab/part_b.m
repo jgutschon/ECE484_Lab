0cfece4a65a56303b62cd3ff55f7b763122b2095
Jon Gutschon <jongutschon123@gmail.com>
2021-12-01 22:46:57 -0500

    add specification output

--- matlab/part_b.m
+++ matlab/part_b.m
@@ -56,20 +56,43 @@ D_2 = c2d(C_2, T);
 sim('general_SD_model.slx', tfinal);
 
 %% LAB 3 PART B %%
-% format long
-% [NUM,DEN]=tfdata(D_2,'v');
-% fprintf('float A2_0 = %.15f;\n', NUM(1));
-% fprintf('float A2_1 = %.15f;\n', NUM(2));
-% fprintf('float B2_0 = %.15f;\n', DEN(1));
-% fprintf('float B2_1 = %.15f;\n', DEN(2));
+format long
+[NUM,DEN]=tfdata(D_2,'v');
+fprintf('float A2_0 = %.15f;\n', NUM(1));
+fprintf('float A2_1 = %.15f;\n', NUM(2));
+fprintf('float B2_0 = %.15f;\n', DEN(1));
+fprintf('float B2_1 = %.15f;\n', DEN(2));
 
-%% Plot
+%% Specifications Output
 % Experimental Data
 t_exp = xlsread('lab3partb.xlsx', 'Sheet1', 'A20002:A39992');
 y_ref_exp = xlsread('lab3partb.xlsx', 'Sheet1', 'B20002:B39992');
 y_exp = xlsread('lab3partb.xlsx', 'Sheet1', 'D20002:D39992');
 theta_ref_exp = xlsread('lab3partb.xlsx', 'Sheet1', 'F20002:F39992');
 
+% Simulated
+range = 40000:50000;
+y_cycle = y(range);
+y_ref_cycle = y_ref(range);
+theta_cycle = theta(range);
+
+theta_max = max(abs(min(theta_cycle)), abs(max(theta_cycle)))
+ref_max = position_offset + position_amplitude;
+os_perc = (max(y_cycle) - ref_max) / position_amplitude * 100
+y_2settling = find(y_cycle < ref_max - position_amplitude * 0.02 | y_cycle > ref_max + position_amplitude * 0.02, 1, 'last') / 1000
+
+% Experimental
+range = 10000:19990;
+y_cycle = y_exp(range);
+y_ref_cycle = y_ref_exp(range);
+theta_cycle = theta_ref_exp(range);
+
+theta_max = max(abs(min(theta_cycle)), abs(max(theta_cycle)))
+ref_max = position_offset + position_amplitude;
+os_perc = (max(y_cycle) - ref_max) / position_amplitude * 100
+y_2settling = find(y_cycle < ref_max - position_amplitude * 0.02 | y_cycle > ref_max + position_amplitude * 0.02, 1, 'last') / 1000
+
+%% Plot
 range = 30000:50000;
 figure(1);
 hold on;
